284e798bf9a9997f336a23c334d0dcc81dcd96e0
Mikko Kouhia <mikko.kouhia@iki.fi>
2023-12-03 07:05:22 +0200

    Add helper for creating new solution

--- advent_of_code/__main__.py
+++ advent_of_code/__main__.py
@@ -7,7 +7,7 @@ import datetime
 import click
 
 from . import __version__
-from .helpers import get_data
+from .helpers import get_data, create_new_template
 
 from .y23 import solutions as solutions_23
 
@@ -77,6 +77,28 @@ def list_puzzles():
         click.echo(f"# {year}")
         for day, cls_ in func_dict.items():
             click.echo(f"{day:2d} {cls_.__name__}")
+
+
+@cli.command
+@click.option(
+    "--year",
+    type=int,
+    default=_today.year,
+    help=f"Event year. Default: this year ({_today.year}).",
+)
+@click.option(
+    "--day",
+    type=int,
+    default=_today.day,
+    help=f"Event day. Default: today (day={_today.day}).",
+)
+def new(year: int, day: int):
+    """Create puzzle template for today."""
+    click.echo(f"Creating new test and development files for {year=}, {day=}:")
+    dev, test = create_new_template(year, day)
+    click.echo(f"- {dev}")
+    click.echo(f"- {test}")
+    click.echo(f"Remember to add import into {dev.parent}/__init__.py")
 
 
 if __name__ == "__main__":
--- advent_of_code/helpers.py
+++ advent_of_code/helpers.py
@@ -51,3 +51,59 @@ def _get_session_id() -> str:
         raise UserWarning("Session ID not found in file .aoc_session_id")
 
     return dotfile.read_text(encoding="utf-8").strip("\n")
+
+
+def create_new_template(year: int, day: int) -> tuple[Path, Path]:
+    """Create new development and test files for a puzzle.
+
+    Returns:
+        Paths to created development and test files.
+    """
+    year_str = f"y{year - 2000}"
+    module_str = f"day{day:02}"
+    class_name = f"Solution{year_str.capitalize()}{module_str.capitalize()}"
+
+    content = {
+        "dev": f'''"""https://adventofcode.com/{year}/day/{day}"""
+
+from ..base import Puzzle
+
+class {class_name}(Puzzle):
+    
+    def part1(self) -> str | int:
+        return super().part1()
+    
+    def part2(self) -> str | int:
+        return super().part2()
+''',
+        "test": f"""import pytest
+
+from advent_of_code.{year_str}.{module_str} import {class_name}
+
+
+@pytest.fixture
+def sample_input() -> str:
+    return ...
+
+
+@pytest.mark.skip
+def test_part1(sample_input: str):
+    assert {class_name}(sample_input).part1() == ...
+
+
+@pytest.mark.skip
+def test_part2(sample_input: str):
+    assert {class_name}(sample_input).part2() == ...
+""",
+    }
+    locations = {
+        "dev": Path("advent_of_code", year_str, f"{module_str}.py"),
+        "test": Path("tests", year_str, f"test_{day:02}.py"),
+    }
+
+    for key, location in locations.items():
+        if not location.exists():
+            location.parent.mkdir(exist_ok=True, parents=True)
+            location.write_text(content[key], encoding="utf-8")
+
+    return tuple(locations.values())
