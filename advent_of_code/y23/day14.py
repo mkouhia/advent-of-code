0fc7a5bd67ec3a4d4d7436e366ba4274d0540da2
Mikko Kouhia <mikko.kouhia@iki.fi>
2023-12-15 05:42:37 +0200

    Part 2

--- advent_of_code/y23/day14.py
+++ advent_of_code/y23/day14.py
@@ -1,5 +1,6 @@
 """https://adventofcode.com/2023/day/14"""
 
+from collections.abc import Iterable
 import itertools
 
 from ..base import Puzzle
@@ -13,47 +14,98 @@ class ParabolicReflectorDish(Puzzle):
         super().__init__(input_text)
         self.rows = self.input_text.strip().splitlines()
 
+    def position_hash(self) -> int:
+        """Returns hash corresponding to the pattern."""
+        return hash(self.pattern)
+
     @property
     def pattern(self) -> str:
         """Get string pattern of the stones."""
-        return '\n'.join(self.rows)
-    
+        return "\n".join("".join(r) for r in self.rows)
+
     def pack_north(self) -> "ParabolicReflectorDish":
-        """Tilt the dish towards north, pack stones to top of board."""
-        cols = ["".join(col) for col in zip(*self.rows)]
-        cols_packed = [self.pack_string(s) for s in cols]
-        self.rows =[''.join(row) for row in zip(*cols_packed)]
+        """Tilt the dish towards north, pack stones to the top of board."""
+        col_iterables = zip(*self.rows)
+        cols_packed = map(self.pack_string, col_iterables)
+        self.rows = list(zip(*cols_packed))
         return self
 
-    # def pack_south(self):
+    def pack_east(self) -> "ParabolicReflectorDish":
+        """Tilt the dish towards east, pack stones to the right of board."""
+        self.rows = [self.pack_string(r, reverse=True) for r in self.rows]
+        return self
 
-    @staticmethod
-    def pack_string(original: str) -> str:
+    def pack_south(self) -> "ParabolicReflectorDish":
+        """Tilt the dish towards south, pack stones to the bottom of board."""
+        col_iterables = zip(*self.rows)
+        cols_packed = (self.pack_string(c, reverse=True) for c in col_iterables)
+        self.rows = list(zip(*cols_packed))
+        return self
+
+    def pack_west(self) -> "ParabolicReflectorDish":
+        """Tilt the dish towards west, pack stones to the left of board."""
+        self.rows = [self.pack_string(r) for r in self.rows]
+        return self
+
+    def pack_cycle(self):
+        """Tilt the dish one cycle; pack north, west, south, east."""
+        self.pack_north()
+        self.pack_west()
+        self.pack_south()
+        self.pack_east()
+        return self
+
+    @classmethod
+    def pack_string(cls, original: Iterable[str], reverse=False) -> str:
         """Take original string, pack O characters to beginnings.
-        
+
         Characters # act as blockers.
         """
+
+        def safe_pack(part: str, reverse: bool) -> str:
+            if reverse:
+                return "." * part.count(".") + "O" * part.count("O")
+            return "O" * part.count("O") + "." * part.count(".")
+
         start_ = sum(1 for _ in itertools.takewhile(lambda c: c == "#", original))
-        end_ = sum(1 for _ in itertools.takewhile(lambda c: c == "#", reversed(original)))
+        ret = start_ * "#"
 
-        ret = start_ * '#' 
+        while "#" in original[start_:]:
+            end2 = original.index("#", start_)
+            part = original[start_:end2]
+            ret += safe_pack(part, reverse) + "#"
+            start_ = end2 + 1
 
-        for part in original[start_:len(original)-end_].split("#"):
-            # TODO check which is faster: this, or sorted(part, key=order.index(k))
-            ret += 'O' * part.count('O') + '.' * part.count('.') + "#"
+        return ret + safe_pack(original[start_:], reverse)
 
-        return ret[:-1] + end_ * '#'
-    
     def total_load(self) -> int:
         """Calculate total load of the reflector dish."""
+        rows = list(self.rows)
         return sum(
-            row_no * row.count('O')
-            for row, row_no in zip(self.rows, range(len(self.rows), 0, -1))
+            row_no * row.count("O")
+            for row, row_no in zip(rows, range(len(rows), 0, -1))
         )
 
     def part1(self) -> str | int:
         """Tilt platform north, calculate total load."""
         return self.pack_north().total_load()
-    
+
     def part2(self) -> str | int:
-        return super().part2()
+        """Find total load after 1e9 cycles."""
+        hashes = []
+        weights = []
+        base_cycles = 0
+        cycle_modulo = 0
+        while not cycle_modulo:
+            hash_ = self.position_hash()
+            if hash_ in hashes:
+                base_cycles = hashes.index(hash_)
+                cycle_modulo = len(hashes) - base_cycles
+                break
+            weights.append(self.total_load())
+            hashes.append(hash_)
+
+            self.pack_cycle()
+
+        idx = int((1e9 - base_cycles) % cycle_modulo + base_cycles)
+        return weights[idx]
