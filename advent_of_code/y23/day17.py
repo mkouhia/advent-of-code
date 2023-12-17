5c96786ca12b77e98997d75413acddba29aa3ef2
Mikko Kouhia <mikko.kouhia@iki.fi>
2023-12-17 18:24:53 +0200

    Day 17, part one.
    This required some guidance from Reddit.

--- advent_of_code/y23/day17.py
+++ advent_of_code/y23/day17.py
@@ -1,108 +1,60 @@
 """https://adventofcode.com/2023/day/17"""
 
-import itertools
-import sys
-from typing import TypeVar
-
-from tqdm import tqdm
+import heapq
 
 from ..base import Puzzle
 
 
 class ClumsyCrucible(Puzzle):
+    
+    """Breadth-first search (BFS) for 2d array navigation.
+    
+    Each cell has an entry cost, and the legs must be of certain length.
+    Therefore, basic Dijkstra's algorithm does not apply.
+    """
+
     def __init__(self, input_text: str) -> None:
         super().__init__(input_text)
-        self.rows = input_text.strip().split("\n")
+        self.rows = [[int(c) for c in row] for row in input_text.strip().split("\n")]
         self.shape = (len(self.rows), len(self.rows[0]))
 
     def part1(self) -> str | int:
         """Least heat loss on the crucible path."""
-        graph = self.construct_graph()
-        dist, _ = dijkstra_algorithm(graph, (0, 0), progress=True)
-        return min(dist[(self.shape[0] - 1, self.shape[1] - 1)].values())
+        return self.search(start_node=(0,0))
 
     def part2(self) -> str | int:
         return super().part2()
 
-    def construct_graph(
-        self,
-    ) -> dict[tuple[int, int], list[tuple[tuple[int, int], int]]]:
-        """Construct graph from puzzle rectangular input."""
-        graph = {}
-        for row in range(self.shape[0]):
-            for col in range(self.shape[1]):
-                graph[(row, col)] = self.neighbours(row, col)
-        return graph
+    def search(self, start_node: tuple[int, int]):
+        """Do a breadth-first search for the minimum distance."""
+        # queue contains (dist, y, x, dy, dx) tuples
+        queue_ = [(0, *start_node, 0, 0)]
+        visited = set()
+        end_node = (self.shape[0]-1, self.shape[1]-1)
 
-    def neighbours(self, i, j) -> list[tuple[tuple[int, int], int]]:
-        """Construct list of neighboring nodes to (i, j).
+        while queue_:
 
-        Returns list of node, distance, direction tuples.
-        """
-        neighbours = []
-        for dy, dx, s in [(0, -1, "<"), (0, 1, ">"), (-1, 0, "^"), (1, 0, "v")]:
-            if (0 <= (ny := i + dy) <= self.shape[0] - 1) and (
-                0 <= (nx := j + dx) <= self.shape[1] - 1
-            ):
-                node = (ny, nx)
-                weight = self.rows[node[0]][node[1]]
-                neighbours.append((node, int(weight), s))
-        return neighbours
-
-
-T = TypeVar("T")
-
-
-def dijkstra_algorithm(
-    graph: dict[T, list[tuple[T, int]]], start_node: T, progress=False
-):
-    queue_ = set(graph.keys())
-
-    # Node: {'ddd': dist} where d are previous directions
-    dist = {start_node: {"": 0}}
-    prev = {}
-
-    pbar = tqdm(total=len(queue_)) if progress else None
-    while queue_:
-        # u ← vertex in Q with min dist[u]
-        # remove u from Q
-        current_min_node = next(
-            node_
-            for node_, _ in sorted(dist.items(), key=lambda p: min(p[1].values()))
-            if node_ in queue_
-        )
-        queue_.remove(current_min_node)
-
-        # for each neighbor v of u still in Q
-        # TODO here, possible neighbours are those that allow shift into that dir
-        neighbors = graph[current_min_node]
-        for neighbor, weight, dir_ in neighbors:
-            # Go through different ways that current node was reached
-            for path_hist, cur_dist in dist[current_min_node].items():
-                if path_hist == dir_ * 3:
-                    continue  # Cannot continue this direction
-                if len(path_hist) > 0 and "".join(sorted([path_hist[0], dir_])) in [
-                    "<>",
-                    "^v",
-                ]:
-                    continue  # Do not cycle back and forth
-
-                new_hist = (
-                    "".join(itertools.takewhile(lambda k: k == dir_, path_hist[::-1]))
-                    + dir_
-                )
-
-                # if neighbor not in queue_:
-                #     continue
-                tentative_value = cur_dist + weight
-                if neighbor not in dist:
-                    dist[neighbor] = {}
-                if tentative_value < dist[neighbor].get(new_hist, sys.maxsize):
-                    dist[neighbor][new_hist] = tentative_value
-                    # We also update the best path to the current node
-                    prev[neighbor] = current_min_node
-
-        if progress:
-            pbar.update()
-
-    return dist, prev
+            dist, y, x, dy_prev, dx_prev = heapq.heappop(queue_)
+            
+            if (y, x) == end_node:
+                return dist
+            if (cur_pt := (y, x, dy_prev, dx_prev)) in visited:
+                continue
+            
+            visited.add(cur_pt)
+            
+            # Next possible directions, 0..3 steps to some other direction
+            for dy, dx in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                if (dy, dx) in [(dy_prev, dx_prev), (-dy_prev, -dx_prev)]:
+                    continue
+                traveled = 0
+                for travel_length in range(1, 3+1):
+                    new_y = y + dy * travel_length
+                    new_x = x + dx * travel_length
+                    if (
+                        (0 <= new_y <= self.shape[0] - 1)
+                        and (0 <= new_x <= self.shape[1] - 1)
+                    ):
+                        traveled += self.rows[new_y][new_x]
+                        # Add here and check visitedness when it comes up in the queue.
+                        heapq.heappush(queue_, (dist + traveled, new_y, new_x, dy, dx))
--- tests/y23/test_17.py
+++ tests/y23/test_17.py
@@ -1,7 +1,6 @@
 import pytest
 
-from advent_of_code.y23.day17 import ClumsyCrucible, dijkstra_algorithm
-from advent_of_code.helpers import TermColour, highlight_by_pos, highlight_regex
+from advent_of_code.y23.day17 import ClumsyCrucible
 
 
 @pytest.fixture
@@ -21,54 +20,6 @@ def sample_input() -> str:
 4322674655533"""
 
 
-def test_graph():
-    s = """123\n456"""
-    expected = {
-        (0, 0): [((0, 1), 2), ((1, 0), 4)],
-        (0, 1): [((0, 0), 1), ((0, 2), 3), ((1, 1), 5)],
-        (0, 2): [((0, 1), 2), ((1, 2), 6)],
-        (1, 0): [((1, 1), 5), ((0, 0), 1)],
-        (1, 1): [((1, 0), 4), ((1, 2), 6), ((0, 1), 2)],
-        (1, 2): [((1, 1), 5), ((0, 2), 3)],
-    }
-    assert ClumsyCrucible(s).construct_graph() == expected
-
-
-def test_dijkstra(sample_input):
-    maze = ClumsyCrucible(sample_input)
-    graph = maze.construct_graph()
-    dist, prev = dijkstra_algorithm(graph, start_node=(0, 0))
-
-    #     path_to_end = []
-    #     node_ = (maze.shape[0] - 1, maze.shape[1] - 1)
-    #     while node_ != (0,0):
-    #         path_to_end.append(node_)
-    #         node_ = prev[node_]
-
-    #     s = highlight_by_pos(sample_input, path_to_end)
-    #     print('This path'.center(40, '-') + '\n' + s)
-
-    expected = """2>>34^>>>1323
-32v>>>35v5623
-32552456v>>54
-3446585845v52
-4546657867v>6
-14385987984v4
-44578769877v6
-36378779796v>
-465496798688v
-456467998645v
-12246868655<v
-25465488877v5
-43226746555v>
-"""
-    s2 = highlight_regex(expected, {r"([\^>v<])": TermColour.OKGREEN})
-    print("Expected".center(40, "-") + "\n" + s2)
-
-    assert min(dist[(12, 12)].values()) == 102
-
-
-@pytest.mark.skip
 def test_part1(sample_input: str):
     assert ClumsyCrucible(sample_input).part1() == 102
 
