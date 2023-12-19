986b85891e60126dbd8d8824051308efa938507e
Mikko Kouhia <mikko.kouhia@iki.fi>
2023-12-19 13:34:45 +0200

    Part 2: range mapping

--- advent_of_code/y23/day19.py
+++ advent_of_code/y23/day19.py
@@ -1,5 +1,6 @@
 """https://adventofcode.com/2023/day/19"""
 
+import operator
 from collections.abc import Iterable
 from dataclasses import dataclass
 
@@ -42,6 +43,62 @@ class Part:
             return getattr(self, attr_name) > value
         raise NotImplementedError
 
+    def __str__(self) -> str:
+        parts = [f"{att}={getattr(self, att)}" for att in "xmas"]
+        return f'{{{",".join(parts)}}}'
+
+
+@dataclass(frozen=True)
+class PartRange:
+    x: tuple[int, ...]
+    m: tuple[int, ...]
+    a: tuple[int, ...]
+    s: tuple[int, ...]
+
+    def split(self, condition: str) -> tuple["PartRange", "PartRange"]:
+        """Split part range by condition.
+
+        Args:
+            condition: Simple equation, such as 'a<2005.
+
+        Returns:
+            Two ranges: first, the range for which the equation is true,
+            then the parts that do not apply to this range.
+        """
+        attr_name = condition[0]
+        comp = condition[1]
+        value = int(condition[2:])
+        assert comp in "<>"
+        oper = operator.lt if comp == "<" else operator.gt
+        accept: list[int] = [i for i in getattr(self, attr_name) if oper(i, value)]
+        discard: list[int] = [i for i in getattr(self, attr_name) if not oper(i, value)]
+
+        other_attr = {n: getattr(self, n) for n in "xmas" if n != attr_name}
+
+        trues = PartRange(**{attr_name: tuple(accept)} | other_attr) if accept else None
+        falses = (
+            PartRange(**{attr_name: tuple(discard)} | other_attr) if discard else None
+        )
+        return trues, falses
+
+    @classmethod
+    def from_single(cls, iterable_):
+        nums_ = tuple(list(iterable_))
+        return cls(x=nums_, m=nums_, a=nums_, s=nums_)
+
+    def n_combinations(self):
+        """Returns amount of distinct part combinations"""
+        return len(self.x) * len(self.m) * len(self.a) * len(self.s)
+
+    def contains(self, part: Part) -> bool:
+        """Part is in part range."""
+        return (
+            part.x in self.x
+            and part.m in self.m
+            and part.a in self.a
+            and part.s in self.s
+        )
+
 
 class Workflow:
 
@@ -50,6 +107,9 @@ class Workflow:
     def __init__(self, name: str, rules: list[str]) -> None:
         self.name = name
         self.rules = rules
+
+    def __str__(self) -> str:
+        return f'{self.name}{{{",".join(self.rules)}}}'
 
     @classmethod
     def from_string(cls, s):
@@ -66,8 +126,25 @@ class Workflow:
             condition, target = rule.split(":")
             if part.evaluate(condition):
                 return target
-        print(self.name, self.rules, part)
         raise NotImplementedError
+
+    def apply_range(self, prange: PartRange) -> Iterable[tuple[str, PartRange]]:
+        """Apply workflow ruleset to part range.
+
+        Yields:
+            Tuples of target to splitted part range.
+        """
+        ret = {}
+        range_remains = prange
+        for rule in self.rules:
+            if ":" not in rule:
+                yield (rule, range_remains)
+                break
+            condition, target = rule.split(":")
+            accepted, rejected = range_remains.split(condition)
+            yield (target, accepted)
+            range_remains = rejected
+        return ret
 
 
 class Aplenty(Puzzle):
@@ -95,9 +172,28 @@ class Aplenty(Puzzle):
             if target == "A":
                 yield part
 
+    def process_range(self, part_range: PartRange) -> Iterable[PartRange]:
+        """Put part ranges through workflows, return ranges that end accepted."""
+        targets = {"in": part_range}
+        while targets:
+            next_key = next(iter(targets.keys()))
+            prange = targets.pop(next_key)
+            flow = self.workflows[next_key]
+
+            for target_, range_ in flow.apply_range(prange):
+                if target_ == "R" or range_ is None:
+                    continue
+                elif target_ == "A":
+                    yield range_
+                elif target_ not in targets:
+                    targets[target_] = range_
+                else:
+                    raise NotImplementedError
+
     def part1(self) -> str | int:
         """Sum of rating numbers for all accepted parts."""
         return sum(p.sum_rating() for p in self.process_parts())
 
     def part2(self) -> str | int:
-        return super().part2()
+        prange = PartRange.from_single(range(1, 4001))
+        return sum(r.n_combinations() for r in self.process_range(prange))
--- tests/y23/test_19.py
+++ tests/y23/test_19.py
@@ -1,6 +1,7 @@
+from functools import reduce
 import pytest
 
-from advent_of_code.y23.day19 import Aplenty, Part, Workflow
+from advent_of_code.y23.day19 import Aplenty, Part, PartRange, Workflow
 
 
 @pytest.fixture
@@ -36,10 +37,64 @@ def test_process_parts(sample_input: str):
     assert len(list(aplenty.process_parts())) == 3
 
 
+def test_split():
+    l = (0, 1, 2, 3, 4, 5)
+    prange = PartRange.from_single(l)
+
+    expected = (
+        PartRange(x=(0, 1, 2), m=l, a=l, s=l),
+        PartRange(x=(3, 4, 5), m=l, a=l, s=l),
+    )
+    assert prange.split("x<3") == expected
+
+
+def test_apply_range():
+    flow = Workflow.from_string("in{x<3:px,qqz}")
+    l = (0, 1, 2, 3, 4, 5)
+    prange = PartRange.from_single(l)
+    expected = {
+        "px": PartRange(x=(0, 1, 2), m=l, a=l, s=l),
+        "qqz": PartRange(x=(3, 4, 5), m=l, a=l, s=l),
+    }
+    assert dict(flow.apply_range(prange)) == expected
+
+
+def test_split_combinations():
+    prange = PartRange.from_single(range(1000))
+    flow = Workflow.from_string("rfg{s<537:gd,x>440:R,A}")
+
+    assert prange.n_combinations() == sum(
+        r.n_combinations() for _, r in flow.apply_range(prange)
+    )
+
+
+def test_process_range(sample_input: str):
+    aplenty = Aplenty(sample_input)
+    ranges_ = {
+        cls_: tuple([getattr(p, cls_) for p in aplenty.parts]) for cls_ in "xmas"
+    }
+    prange = PartRange(**ranges_)
+
+    accepted_parts = list(aplenty.process_parts())
+    accepted_ranges = list(aplenty.process_range(prange))
+
+    for r in accepted_ranges:
+        print(r.n_combinations(), r)
+
+    for p in accepted_parts:
+        print(p)
+        for r in accepted_ranges:
+            print(r.contains(p), r)
+            if r.contains(p):
+                break
+        else:
+            assert False, f"None of the ranges contain the accepted part {p}"
+    assert all([any(r.contains(p) for r in accepted_ranges) for p in accepted_parts])
+
+
 def test_part1(sample_input: str):
     assert Aplenty(sample_input).part1() == 19114
 
 
-@pytest.mark.skip
 def test_part2(sample_input: str):
-    assert Aplenty(sample_input).part2() == ...
+    assert Aplenty(sample_input).part2() == 167409079868000
