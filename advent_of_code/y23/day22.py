ab12063a46efe2fa371210ac43200b1e0b75b0fb
Mikko Kouhia <mikko.kouhia@iki.fi>
2023-12-22 14:41:48 +0200

    Part 2

--- advent_of_code/y23/day22.py
+++ advent_of_code/y23/day22.py
@@ -1,50 +1,55 @@
 """https://adventofcode.com/2023/day/22"""
 
-from collections import namedtuple
-import heapq
+from collections import deque, namedtuple
 import itertools
 from collections.abc import Iterable
 from dataclasses import dataclass
 
 from ..base import Puzzle
 
+
 @dataclass
 class Brick:
+    """A rectangular brick taking some amount of x,y,z space."""
+
     x: tuple[int, int]
     y: tuple[int, int]
     z: tuple[int, int]
 
     @classmethod
     def from_spec(cls, spec: str) -> "Brick":
-        parts = [map(int, pos.split(',')) for pos in spec.split("~")]
+        parts = [map(int, pos.split(",")) for pos in spec.split("~")]
         x, y, z = list(zip(*parts))
         return cls(x, y, z)
-    
+
     def __lt__(self, other: "Brick"):
         return self.z[0] < other.z[0]
 
     def xy_span(self) -> Iterable[tuple[int, int]]:
         yield from itertools.product(
-            range(self.x[0], self.x[1]+ 1),
-            range(self.y[0], self.y[1]+ 1)
+            range(self.x[0], self.x[1] + 1), range(self.y[0], self.y[1] + 1)
         )
 
     @property
     def height(self) -> int:
+        """Height in z direction"""
         return self.z[1] - self.z[0] + 1
 
 
 class SandSlabs(Puzzle):
 
+    """Graph traversal problem, given 3d blocks."""
+
     def __init__(self, input_text: str) -> None:
         super().__init__(input_text)
         self.bricks = [Brick.from_spec(s) for s in input_text.strip().splitlines()]
-    
+        self.support_graph, self.brick_dic = self.construct_graph()
+        self.depend_graph = self.reverse_graph(self.support_graph)
+
     def construct_graph(self):
+        """Create graph: brick -> bricks supported by this brick."""
         brick_dic = {i: brick for i, brick in enumerate(self.bricks)}
-        graph = {
-            -1: set()
-        }
+        graph = {-1: set()}
 
         BlockHeight = namedtuple("BlockHeight", ["z", "block_id"])
         # For each xy cell, order bricks in the order that they would fall
@@ -57,8 +62,7 @@ class SandSlabs(Puzzle):
                     continue
                 if max_heights[xy].z > prev_max:
                     prev_max = max_heights[xy].z
-            
-            
+
             # Place this new brick to that current max height
             for xy in brick.xy_span():
                 # Create connections to directly supporting bricks
@@ -74,9 +78,10 @@ class SandSlabs(Puzzle):
             graph[id_] = set()
 
         return graph, brick_dic
-    
+
     @staticmethod
-    def reverse_graph(graph: dict):
+    def reverse_graph(graph: dict[int, set[int]]) -> dict[int, set[int]]:
+        """Reverse graph - returns: brick -> bricks that support this brick."""
         ret = {}
         for source, targets in graph.items():
             for target in targets:
@@ -84,13 +89,12 @@ class SandSlabs(Puzzle):
                     ret[target] = set()
 
                 ret[target].add(source)
-        
+
         return ret
-    
-    def can_be_removed(self, graph: dict[int, set[int]]) -> list[int]:
+
+    def can_be_removed(self) -> list[int]:
         ret = []
-        block_is_supported = self.reverse_graph(graph)
-        for node_id, supported_ids in graph.items():
+        for node_id, supported_ids in self.support_graph.items():
             # Leaves can be removed
             if not supported_ids:
                 ret.append(node_id)
@@ -99,7 +103,7 @@ class SandSlabs(Puzzle):
             # Blocks with siblings can be removed
             all_blocks_have_other_supports = True
             for supported_ in supported_ids:
-                supporting_blocks = block_is_supported[supported_]
+                supporting_blocks = self.depend_graph[supported_]
                 if len(supporting_blocks) == 1:
                     all_blocks_have_other_supports = False
                     break
@@ -109,10 +113,40 @@ class SandSlabs(Puzzle):
         return ret
 
     def part1(self) -> str | int:
-        block_supports, _ = self.construct_graph()
-        rem_ids = self.can_be_removed(block_supports)
-
+        """How many individual blocks could be removed, without affectin other blocks."""
+        rem_ids = self.can_be_removed()
         return len(rem_ids)
-    
+
+    def _fall_count(self, id_: int):
+        """Calculate how many other bricks would fall if this were removed."""
+        block_is_supported = self.depend_graph.copy()
+        count = 0
+        would_fall = deque(list(self.support_graph[id_]))
+
+        removed = {id_}
+        removed_at_prev_try = {}
+        while would_fall:
+            try_next = would_fall.popleft()
+            if try_next in removed:
+                continue
+
+            if len(block_is_supported[try_next] - removed) > 0:
+                # Still supported...
+                removed_now = hash(tuple(removed))
+                if removed_at_prev_try.get(try_next, None) != removed_now:
+                    # Try again after some another drops
+                    would_fall.append(try_next)
+
+                removed_at_prev_try[try_next] = removed_now
+                continue
+
+            # This block is falling
+            removed.add(try_next)
+            count += 1
+            would_fall.extend(self.support_graph[try_next])
+
+        return count
+
     def part2(self) -> str | int:
-        return super().part2()
+        """Sum of fall counts for each individual brick."""
+        return sum(self._fall_count(id_) for id_ in self.brick_dic)
--- tests/y23/test_22.py
+++ tests/y23/test_22.py
@@ -1,6 +1,6 @@
 import pytest
 
-from advent_of_code.y23.day22 import Brick, SandSlabs, print_graph
+from advent_of_code.y23.day22 import Brick, SandSlabs
 
 
 @pytest.fixture
@@ -14,13 +14,14 @@ def sample_input() -> str:
 1,1,8~1,1,9
 """
 
+
 def test_brick_from_spec():
-    assert Brick.from_spec("1,0,1~1,2,1") == Brick((1,1), (0,2), (1,1))
+    assert Brick.from_spec("1,0,1~1,2,1") == Brick((1, 1), (0, 2), (1, 1))
+
 
 def test_part1(sample_input: str):
     assert SandSlabs(sample_input).part1() == 5
 
 
-@pytest.mark.skip
 def test_part2(sample_input: str):
-    assert SandSlabs(sample_input).part2() == ...
+    assert SandSlabs(sample_input).part2() == 7
