3e1a41ba5d4ecc449ac50b3f1f46df13ba1379e6
Mikko Kouhia <mikko.kouhia@iki.fi>
2024-01-01 17:34:45 +0200

    Day 21 part 2

--- advent_of_code/y23/day21.py
+++ advent_of_code/y23/day21.py
@@ -4,14 +4,13 @@ from ..base import Puzzle
 
 
 class StepCounter(Puzzle):
-    def __init__(self, input_text: str, n_steps=64) -> None:
+    def __init__(self, input_text: str) -> None:
         super().__init__(input_text)
         self.rows = self.input_text.strip().splitlines()
         self.shape = (len(self.rows), len(self.rows[0]))
         for i, row in enumerate(self.rows):
             if "S" in row:
                 self.start_pos = (i, row.index("S"))
-        self.n_steps = n_steps
 
     def take_step(self, locations: set[tuple[int, int]], not_allowed="#"):
         destinations = set()
@@ -26,17 +25,76 @@ class StepCounter(Puzzle):
                 ):
                     continue
 
-                if self.rows[new_y][new_x] in not_allowed:
+                if self.rows[new_y][new_x] == not_allowed:
                     continue
 
                 destinations.add((new_y, new_x))
         return destinations
 
-    def part1(self) -> str | int:
-        locations = {self.start_pos}
-        for _ in range(self.n_steps):
+    def n_loc(self, start_pos: tuple[int, int], n_steps: int):
+        locations = {start_pos}
+        for _ in range(n_steps):
             locations = self.take_step(locations)
         return len(locations)
 
+    def part1(self) -> str | int:
+        return self.n_loc(self.start_pos, 64)
+
+    @classmethod
+    def after_n(cls, n) -> tuple[int, int, int, int]:
+        ret = {
+            "a_even": (n - 1) ** 2,
+            "a_odd": n**2,
+            "b_": 1,
+            "c_": n - 1,
+            "d_": n,
+        }
+
+        return ret
+
+    def a_cnt(self, n):
+        if n == 1:
+            return (1, 0)
+
+    def locs_after(self, n_steps):
+        dist = self.shape[1]
+        dist_h = self.start_pos[1]
+        n = n_steps // dist
+        assert dist * n + dist_h == n_steps
+        assert n >= 1
+
+        n_loc = self.after_n(n)
+
+        offset = 1 - dist_h % 2
+        steps = {
+            "a_odd": self.n_loc(self.start_pos, dist + (1 - offset)),
+            "a_even": self.n_loc(self.start_pos, dist + offset),
+            "b_N": self.n_loc((dist, self.start_pos[1]), dist),
+            "b_S": self.n_loc((-1, self.start_pos[1]), dist),
+            "b_E": self.n_loc((self.start_pos[0], -1), dist),
+            "b_W": self.n_loc((self.start_pos[0], dist), dist),
+            "d_NE": self.n_loc((dist - 1, -1), dist_h),
+            "c_NE": self.n_loc((dist - 1, -1), dist_h + dist),
+            "d_SE": self.n_loc((0, -1), dist_h),
+            "c_SE": self.n_loc((0, -1), dist_h + dist),
+            "d_NW": self.n_loc((dist - 1, dist), dist_h),
+            "c_NW": self.n_loc((dist - 1, dist), dist_h + dist),
+            "d_SW": self.n_loc((0, dist), dist_h),
+            "c_SW": self.n_loc((0, dist), dist_h + dist),
+        }
+
+        ret = 0
+        for pos in ["a_odd", "a_even"]:
+            ret += n_loc[pos] * steps[pos]
+
+        for dir in "NESW":
+            ret += n_loc["b_"] * steps[f"b_{dir}"]
+
+        for dir in ["NE", "SE", "SW", "NW"]:
+            ret += n_loc["c_"] * steps[f"c_{dir}"]
+            ret += n_loc["d_"] * steps[f"d_{dir}"]
+
+        return ret
+
     def part2(self) -> str | int:
-        return super().part2()
+        return self.locs_after(26501365)
--- tests/y23/test_21.py
+++ tests/y23/test_21.py
@@ -20,9 +20,128 @@ def sample_input() -> str:
 
 
 def test_part1(sample_input: str):
-    assert StepCounter(sample_input, n_steps=6).part1() == 16
+    sc = StepCounter(sample_input)
+    assert sc.n_loc(sc.start_pos, 6) == 16
 
 
-@pytest.mark.skip
-def test_part2(sample_input: str):
-    assert StepCounter(sample_input).part2() == ...
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (1, {"a_odd": 1, "a_even": 0, "b_": 1, "c_": 0, "d_": 1}),
+        (2, {"a_odd": 4, "a_even": 1, "b_": 1, "c_": 1, "d_": 2}),
+        (3, {"a_odd": 9, "a_even": 4, "b_": 1, "c_": 2, "d_": 3}),
+        (4, {"a_odd": 16, "a_even": 9, "b_": 1, "c_": 3, "d_": 4}),
+    ],
+)
+def test_pos(n, expected):
+    assert StepCounter.after_n(n) == expected
+
+
+@pytest.mark.parametrize("n,expected", [(4, 25), (7, 64), (26501365, 702322399865956)])
+def test_part2(n, expected):
+    s = "...\n.S.\n..."
+    assert StepCounter(s).locs_after(n) == expected
+
+
+@pytest.fixture
+def reddit_sample():
+    return """.................
+..#..............
+...##........###.
+.............##..
+..#....#.#.......
+.......#.........
+......##.##......
+...##.#.....#....
+........S........
+....#....###.#...
+......#..#.#.....
+.....#.#..#......
+.#...............
+.#.....#.#....#..
+...#.........#.#.
+...........#..#..
+................."""
+
+
+@pytest.fixture
+def sample_odd():
+    """Same as Reddit sample, but 2 rows/cols less -> d/2=7"""
+    return """...............
+..#............
+...##......###.
+...........##..
+..#............
+...............
+......#.#......
+.......S.......
+......#..#.....
+.....#..#......
+.#.............
+.#..........#..
+...#.......#.#.
+.........#..#..
+..............."""
+
+
+@pytest.fixture
+def sample_stacked(reddit_sample):
+    return stack_s(reddit_sample, 9)
+
+
+@pytest.fixture
+def sample_odd_stacked(sample_odd):
+    return stack_s(sample_odd, 9)
+
+
+@pytest.mark.parametrize(
+    "n,expected",
+    [
+        # (7, 52),
+        # (8, 68),
+        (25, 576),
+        (42, 1576),
+        (59, 3068),
+        (76, 5052),
+        (1180148, 1185525742508),
+    ],
+)
+def test_part2(n, expected, reddit_sample):
+    assert StepCounter(reddit_sample).locs_after(n) == expected
+
+
+@pytest.mark.parametrize(
+    "n,expected",
+    [
+        (25, 576),
+        (42, 1576),
+        (59, 3068),
+        (76, 5052),
+    ],
+)
+def test_expanded(n, expected, sample_stacked):
+    sc = StepCounter(sample_stacked)
+    count_stacked = sc.n_loc(sc.start_pos, n)
+    assert count_stacked == expected
+
+
+@pytest.mark.parametrize("n", [1, 2, 3, 4])
+def test_odd(n, sample_odd, sample_odd_stacked):
+    k = len(sample_odd.splitlines())
+    steps = n * k + k // 2
+    part2 = StepCounter(sample_odd).locs_after(steps)
+    sc = StepCounter(sample_odd_stacked)
+    count_stacked = sc.n_loc(sc.start_pos, steps)
+    assert count_stacked == part2
+
+
+def stack_s(s: str, n: int) -> str:
+    lines = s.replace("S", ".").strip().splitlines()
+    lines_w = list(map("".join, zip(*([lines] * n))))
+    lines_x = lines_w * n
+    pos = len(lines_x) // 2
+
+    midline = list(lines_x[pos])
+    midline[pos] = "S"
+    lines_x[pos] = "".join(midline)
+    return "\n".join(lines_x)
